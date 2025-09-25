1b30095aefd73e2fccbf050e7ad58fc18a55a889
PPDev2326 <paoloperezinga@gmail.com>
2025-09-24 19:19:19 -0500

    que devuelve excel pruebas

--- lib/Helper/_Excel.py
+++ lib/Helper/_Excel.py
@@ -21,8 +21,9 @@ class Excel:
             forms.alert("No se seleccionó ningún archivo.", exitscript=True)
             return []
         
-        datos = xl.load(ruta, sheets=str(hoja), headers=encabezados)
-        filas = datos.get(str(hoja), {}).get('rows', [])
+        datos = xl.load(ruta, headers=False)
+        print(type(datos))
+        print(datos.keys())
         
         return filas
     

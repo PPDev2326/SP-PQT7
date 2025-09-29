f3e9b3e5700bd55b15812c701979ebaf482d5202
PPDev2326 <paoloperezinga@gmail.com>
2025-09-29 17:09:01 -0500

    primeras pruebas de COBie componenet y el metodo SetParameter modificado

--- PQT7.tab/COBie.panel/col1.stack/component.pushbutton/script.py
+++ PQT7.tab/COBie.panel/col1.stack/component.pushbutton/script.py
@@ -2,28 +2,66 @@
 __title__ = "COBie\nComponent"
 
 # ==== Obtenemos la librerias necesarias ====
-from Autodesk.Revit.DB import Transaction, ElementId, StorageType, FamilyInstance
+from Autodesk.Revit.DB import Transaction, ElementId, StorageType, FamilyInstance, ElementType, BuiltInParameter
 from Autodesk.Revit.UI import TaskDialog
 from Autodesk.Revit.Exceptions import OperationCanceledException
 from Autodesk.Revit.UI.Selection import ObjectType
 from pyrevit import script, revit, forms
 from Extensions._Modulo import obtener_nombre_archivo, validar_nombre
+from Extensions._RevitAPI import GetParameterAPI, get_param_value, getParameter
 from DBRepositories.SchoolRepository import ColegiosRepository
+from DBRepositories.SpecialtiesRepository import SpecialtiesRepository
 
 nombre_archivo = obtener_nombre_archivo()
 if not validar_nombre(nombre_archivo):
     script.exit()
 
+# ==== Metodo para validar valor de parametro vacío o n/a ====
+def get_first_valid_parameter(elem, names):
+    """Devuelve el primer valor válido encontrado en la lista de parámetros."""
+    invalid_values = {None, ""}
+    for name in names:
+        param = getParameter(elem, name)
+        if param:
+            value = get_param_value(param)
+            if isinstance(value, str):
+                if value.strip().lower() in {"", "n/a"}:
+                    continue
+            if value not in invalid_values:
+                return value
+    return None
+
+
+# ==== Metodo para dividir una cadena ====
+def divide_string(text, idx, character_divider=None, compare=None, value_default=None):
+    """
+    Divide un string en partes usando un separador y devuelve el elemento en la posición idx.
+    Si el texto completo coincide con 'compare', devuelve 'value_default'.
+    """
+    if not text:
+        return ""
+    
+    # Verificar coincidencia antes de dividir
+    if compare and text.strip().casefold() == compare.casefold():
+        return value_default
+    
+    parts = text.split(character_divider) if character_divider else text.split()
+    if idx < 0 or idx >= len(parts):
+        return ""
+    
+    return parts[idx]
+
 # ==== Obtener el documento activo ====
 doc = revit.doc
 ui_doc = revit.uidoc
 
-# ==== Seleccionar referencias de elementos ====
+# ==== Seleccionar elementos y obtenemos tipo Reference de los elementos ====
 try:
     references = ui_doc.Selection.PickObjects(ObjectType.Element)
 except OperationCanceledException:
     forms.alert("Operación cancelada: no se seleccionaron elementos para procesar COBie.Component",
                 title="Cancelación")
+    script.exit()
 
 # ==== Datos fijos ====
 CREATED_ON = "2025-08-04T11:59:30"
@@ -34,45 +72,36 @@ school_repo_object = ColegiosRepository()
 school_object = school_repo_object.codigo_colegio(doc)
 school = None
 created_by = None
+warranty_start_date = None
 
+# ==== Verificamos si el colegio existe
 if school_object:
     school = school_object.name
     created_by = school_object.created_by
+    warranty_start_date = school_object.warranty_start_date.component_warranty
+
+# ==== Instanciamos la especialidad correspondiente al modelo ====
+specialty_repo = SpecialtiesRepository()
+specialty_object = specialty_repo.get_specialty_by_document(doc)
+specialty = None
+
+if specialty_object:
+    specialty = specialty_object.name
 
 count = 0
 
 with revit.Transaction("Transfiere datos a Parametros COBieComponent"):
 
     for reference in references:
-        element = doc.GetElement(reference)
-        uniqueId = element.UniqueId
-        id_param = element.Id.IntegerValue
+        # ==== Obtenemos el elemento seleccionado ====
+        element_object = doc.GetElement(reference)
 
-        # Validar el parámetro COBie
-        cobie = element.LookupParameter("COBie")
-        if not (cobie and not cobie.IsReadOnly and cobie.StorageType == StorageType.Integer and cobie.AsInteger() == 1):
-            continue
+        elementos_a_procesar = [element_object]
 
-        # Obtener tipo de elemento
-        element_type_id = element.GetTypeId()
-        if element_type_id == ElementId.InvalidElementId:
-            print("El elemento " + str(id_param) + " no tiene un tipo asociado.")
-            continue
-        element_type = doc.GetElement(element_type_id)
-
-        # Obtener valores de parámetros del elemento y del tipo
-        def obtener_valor(elem, nombre_parametro):
-            param = elem.LookupParameter(nombre_parametro)
-            if param and param.StorageType == StorageType.String:
-                return param.AsString() or ""
-            return ""
-
-        elementos_a_procesar = [element]
-
-        # Verificar si tiene subcomponentes (solo si es FamilyInstance)
-        if isinstance(element, FamilyInstance):
+        # ==== Verificar si tiene subcomponentes (solo si es FamilyInstance) ====
+        if isinstance(element_object, FamilyInstance):
             try:
-                sub_ids = element.GetSubComponentIds()
+                sub_ids = element_object.GetSubComponentIds()
                 if sub_ids:
                     for sid in sub_ids:
                         sub_elem = doc.GetElement(sid)
@@ -82,27 +111,75 @@ with revit.Transaction("Transfiere datos a Parametros COBieComponent"):
                 pass
 
         for elem in elementos_a_procesar:
-            id_elem = elem.Id.IntegerValue
             uid_elem = elem.UniqueId
-            nrm = obtener_valor(elem, "Descripción de partida NRM")
-            ambiente = obtener_valor(elem, "Ambiente")
-            descripcion = obtener_valor(elem, "Descripción de elemento")
-            instalacion = obtener_valor(elem, "Fecha ejecutada")
-            activo = obtener_valor(elem, "Activo")
-            serial_number = obtener_serial_number(activo, str(id_elem))
+            
+            # ==== Validar el parámetro COBie ====
+            cobie = elem.LookupParameter("COBie")
+            if not (cobie and not cobie.IsReadOnly and cobie.StorageType == StorageType.Integer and cobie.AsInteger() == 1):
+                continue
+            
+            # ==== Obtenemos el nivel del elemento ====
+            level_param_value = get_param_value(getParameter(elem, "NIVEL DE ELEMENTO"))
+            level = divide_string(level_param_value, 1)
+            
+            # ==== Obtenemos la categoria del elemento y Verificamos que el elemento tenga categoria ====
+            elem_category_object = elem.Category
+            name_category = elem_category_object.Name if elem_category_object else "Sin categoria"
+            
+
+            id_elem = elem.Id.IntegerValue
+            
+            # ==== Obtenemos el tipo del elemento seleccionado y su nombre ====
+            element_type_object_id = elem.GetTypeId()             # => Obtenemos el ElementId del elemento
+            if element_type_object_id == ElementId.InvalidElementId:
+                print("El elemento id" + str(id_elem) + " no tiene un tipo asociado.")
+                continue
+            el_type_object = doc.GetElement(element_type_object_id)     # => Obtenemos el Tipo de elemento por medio de su ID
+            param_object_type = GetParameterAPI(el_type_object, BuiltInParameter.SYMBOL_NAME_PARAM)
+            name_type = get_param_value(param_object_type)
+            pr_number = get_param_value(getParameter(elem, "Classification.Uniclass.Pr.Number"))
+
+            # ==== Obtenemos la familia del elemento y Verificamos que exista ====
+            family_name = el_type_object.FamilyName if isinstance(el_type_object, ElementType) else "Sin familia"
+
+            # ==== Obtenemos la zonificacion del MBR ====
+            zonification_value = get_param_value(getParameter(elem, "S&P_ZONIFICACION"))
+            mbr_value = divide_string(zonification_value, 1, compare="sitio", value_default="000")
+            
+            # ==== Obtenemos el ambiente del elemento ====
+            ambiente_object = getParameter(elem, "S&P_AMBIENTE")
+            ambiente = get_param_value(ambiente_object)
+            
+            # ==== Obtenemos el codigo del elemento ====
+            code_elem = get_param_value(getParameter(elem, "S&P_CODIGO DE ELEMENTO"))
+            if code_elem not in (None, "", "n/a"):
+                code_elem
+            
+            # ==== Obtenemos el parametro partida N°xx de acuerdo a la especialidad ====
+            if specialty in ["INSTALACIONES SANITARIAS", "COMUNICACIONES"]:
+                description = get_first_valid_parameter(
+                    elem,
+                    ["S&P_DESCRIPCION PARTIDA N°2", "S&P_DESCRIPCION PARTIDA N°1"]
+                )
+            else:
+                description = get_first_valid_parameter(
+                    elem,
+                    ["S&P_DESCRIPCION PARTIDA N°1"]
+                )
+
 
             parametros = {
-                "COBie.Component.Name": str(id_elem) + "_" + str(nrm),
-                "COBie.CreatedBy": str(CREATED_ON),
-                "COBie.CreatedOn": str(DATE),
-                "COBie.Component.Space": str(ambiente),
-                "COBie.Component.Description": str(descripcion),
-                "COBie.Component.SerialNumber": str(serial_number),
-                "COBie.Component.InstallationDate": str(instalacion) + str(EXECUTION_TIME),
-                "COBie.Component.WarrantyStartDate": str(WARRANTY_DATE),
-                "COBie.Component.TagNumber": str(id_elem),
-                "COBie.Component.BarCode": str(numero_conts) + str(id_elem),
-                "COBie.Component.AssetIdentifier": str(uid_elem)
+                "COBie.Component.Name": "{} : {} : {} : {}".format(name_category, family_name, name_type, id_elem),
+                "COBie.CreatedBy": created_by,
+                "COBie.CreatedOn": CREATED_ON,
+                "COBie.Component.Space": ambiente,
+                "COBie.Component.Description": description,
+                "COBie.Component.SerialNumber": "{} {}".format(code_elem, id_elem),
+                "COBie.Component.InstallationDate": "",
+                "COBie.Component.WarrantyStartDate": warranty_start_date,
+                "COBie.Component.TagNumber": "",
+                "COBie.Component.BarCode": "",
+                "COBie.Component.AssetIdentifier": "{}-ZZ-{}-".format(mbr_value, level)
             }
 
             for param_name, value in parametros.items():
--- lib/Extensions/_RevitAPI.py
+++ lib/Extensions/_RevitAPI.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from Autodesk.Revit.DB import BuiltInParameter, Element, StorageType
+from Autodesk.Revit.DB import BuiltInParameter, Element, StorageType, ElementId
 
 def getParameter(element, name):
     """Obtiene un parametro compartido si no es de solo lectura.
@@ -18,12 +18,27 @@ def SetParameter(parameter, new_value):
     :return: True si se seteó correctamente, False si fue de solo lectura o inválido."""
     if parameter and not parameter.IsReadOnly:
         try:
-            parameter.Set(new_value)
+            if parameter.StorageType == StorageType.String:
+                parameter.Set(str(new_value))
+            elif parameter.StorageType == StorageType.Double:
+                parameter.Set(float(new_value))
+            elif parameter.StorageType == StorageType.Integer:
+                parameter.Set(int(new_value))
+            elif parameter.StorageType == StorageType.ElementId:
+                if isinstance(new_value, ElementId):
+                    parameter.Set(new_value)
             return True
-        
         except Exception as e:
-            print("Error al setear el parametro: ", e)
+            print("Error al setear el parámetro:", e)
     return False
+    # if parameter and not parameter.IsReadOnly:
+    #     try:
+    #         parameter.Set(new_value)
+    #         return True
+        
+    #     except Exception as e:
+    #         print("Error al setear el parametro: ", e)
+    # return False
 
 def GetParameterAPI(element, built_param):
     """

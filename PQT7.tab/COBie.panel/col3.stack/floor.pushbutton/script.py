ec565cc4fa6c736d2d1501f8f0586e7ad5817ee1
PPDev2326 <paoloperezinga@gmail.com>
2025-10-01 10:03:02 -0500

    haciendo pruebas de cobie floor

--- PQT7.tab/COBie.panel/col3.stack/floor.pushbutton/script.py
+++ PQT7.tab/COBie.panel/col3.stack/floor.pushbutton/script.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
 __title__ = "COBie Floor"
+
+# ========== Obtenemos las librerias necesarias ==========
+from Autodesk.Revit.DB import FilteredElementCollector, Level
+from Autodesk.Revit.UI import TaskDialog
+from pyrevit import script, revit, forms
+
+# ==== obtenemos el documento y el uidocument del modelo activo ====
+doc = revit.doc
+uidoc = revit.uidoc
+
+# ==== Obtenemos los niveles del modelo con el FilteredElementCollector ====
+fec = FilteredElementCollector(doc)
+list_levels = fec.OfClass(Level).WhereElementIsNotElementType().ToElements()
+
+print(list_levels)
